c35c5f18d1284cc08bd09ab518ad605a08773b4e
Soonly-T <intell1slt@gmail.com>
2025-04-05 10:08:18 +0700

    minor changes

--- frontend/expenses_app/lib/screens/start.dart
+++ frontend/expenses_app/lib/screens/start.dart
@@ -1,35 +1,107 @@
 import 'package:flutter/material.dart';
 
-class StartScreen extends StatelessWidget {
+const double spaceHeight = 32;
+
+class StartScreen extends StatefulWidget {
+  const StartScreen({Key? key}) : super(key: key);
+
   @override
+  State<StartScreen> createState() => _StartScreenState();
+}
+
+class _StartScreenState extends State<StartScreen> {
+  final loginController = TextEditingController();
+  final passwordController = TextEditingController();
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    loginController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  void debugPrintCredentials() {
+    print("printing credentials");
+    print('Login: ${loginController.text}');
+    print('Password: ${passwordController.text}');
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Start Screen')),
+      appBar: AppBar(),
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment:
+              CrossAxisAlignment
+                  .stretch, // Use stretch to make children take full width
           children: [
-            Row(
+            const Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Center(child: Text('Welcome to the Expense Tracker App!')),
+                Text(
+                  'Expense Tracker App',
+                  style: TextStyle(
+                    fontSize: 32,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ],
             ),
-            SizedBox(height: 20),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Username or Email',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 20),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-              obscureText: true,
+            const SizedBox(height: spaceHeight),
+            LayoutBuilder(
+              builder: (context, constraints) {
+                final double width = constraints.maxWidth * 0.75;
+                return Column(
+                  children: [
+                    SizedBox(
+                      width: width,
+                      child: TextFormField(
+                        controller: loginController,
+                        decoration: InputDecoration(
+                          labelText: 'Username or Email',
+                          border: OutlineInputBorder(),
+                        ),
+                        obscureText: false,
+                      ),
+                    ),
+                    const SizedBox(height: spaceHeight),
+                    SizedBox(
+                      width: width,
+                      child: TextFormField(
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          border: OutlineInputBorder(),
+                        ),
+                        obscureText: true,
+                      ),
+                    ),
+                    const SizedBox(height: spaceHeight),
+                    SizedBox(
+                      width: width,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              debugPrintCredentials();
+                            },
+                            child: const Text('Login'),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              // Handle login action
+                            },
+                            child: const Text('Sign Up'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
           ],
         ),
